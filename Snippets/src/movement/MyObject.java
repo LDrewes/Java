fef0728aa0ac8835d8246ebbb641927a52ea6674
Laura Drewes <l.drewes@aventus.nl>
2018-03-15 09:00:04 +0100

    Movement demo verbeterd

--- Snippets/src/movement/MyObject.java
+++ Snippets/src/movement/MyObject.java
@@ -24,22 +24,33 @@ public class MyObject extends JLabel {
 		this.setOpaque(true);
 		this.setBackground(Color.WHITE);
 		this.setBounds(275, 10, 50, 50);
+		
 		timer = new Timer(10, new MoveHandler());
 		timer.start();
 	}
 
 	private void moveDown() {
-		System.out.println("...move down ("+id+")");
+		//System.out.println("...move down ("+id+")");
 		this.setBounds(getX(), getY() + 1, 50, 50);
+		
+		if (this.getY() > 100) {
+			System.out.println("Te laag > verwijder het object");
+			timer.stop();
+			((MyPanel) this.getParent()).removeObject(this);
+		}
 	}
 
 	class MoveHandler implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			// System.out.println("Move...");
 			moveDown();
 		}
+	}
+	
+	@Override
+	public String toString() {
+		return "MyObject " + id;
 	}
 
 }
